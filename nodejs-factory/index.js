d52448e512d0f060f4fbc6c8c97efdd381a4dd7d
Alexandra Turian <alexandra.turian@endava.com>
2020-03-03 12:30:35 +0200

    node js factory example

---
+++ nodejs-factory/index.js
@@ -0,0 +1,44 @@
+// Example on how to create your own server: http or https 
+
+const URL = require('url').URL;
+const http = require('http');
+const https = require('https');
+
+function ServerFactory() {
+	let server;
+	this.createServer = function(protocol) {
+		if (protocol === 'http:') {
+			server = new HttpServer();
+		} else {
+			server = new HttpsServer();
+		}
+		server.protocol = protocol;
+
+		return server;
+	}
+}
+
+var HttpServer = function () {
+	return http.createServer(listener);
+}
+
+var HttpsServer = function () {
+	// use options to set
+	let options = {};
+	// certificates can be read from local files
+	// options.key = readFile(pathToFileWithKey);
+	// options.certificate = readFile(pathToFileWithCertificate)';
+
+	return https.createServer(options, listener);
+}
+
+function listener(req, res) {
+	// log incoming requests to server
+	console.log(new Date().toISOString(), req.url, req.headers);
+}
+
+let url = new URL(process.argv[2]);
+let server = (new ServerFactory).createServer(url.protocol);
+
+// Set your server to listen on port 8082 with previously set listener
+server.listen('8082');
